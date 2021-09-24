292a8c4125aa64674dfc63e025c1179455731b44
lauxie <lxie30@stuy.edu>
2021-09-23 23:54:48 -0400

    functions + tests

--- CtoFTester.java
+++ CtoFTester.java
@@ -3,13 +3,17 @@
 // both functions should return double
 
 
-class CtoFTester.java {
-    public double celsiustoFahrenheit(double celsius) {
+public class CtoFTester {
+    public static double celsiustoFahrenheit(double celsius) {
       double fahrenheit = (celsius * 1.8) + 32;
       return fahrenheit;
     }
-    public double fahrenheitToCelsius(double fahrenheit) {
+    public static double fahrenheitToCelsius(double fahrenheit) {
       double celsius = ((fahrenheit - 32)/1.8);
       return celsius;
+    }
+    public static void main (String[] args) {
+      System.out.println(celsiustoFahrenheit(0));
+      System.out.println(fahrenheitToCelsius(32));
     }
 }
